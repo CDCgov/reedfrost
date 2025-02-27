dee946d10466dad17c5465fb8e602a8723a87cc5
Scott Olesen <ulp7@cdc.gov>
2025-02-27 15:29:26 -0500

    repair tests

--- tests/test_reed_frost.py
+++ tests/test_reed_frost.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-from pytest import approx
 
 import reedfrost as rf
 
@@ -9,9 +8,9 @@ def test_pmf_2():
     """For 1 infected and 2 susceptible, do the math by hand"""
 
     for p in [0.1, 0.7]:
-        assert rf.pmf(0, 2, p, m=1) == approx((1 - p) ** 2, abs=1e-6)
-        assert rf.pmf(1, 2, p, m=1) == approx(2 * p * (1 - p) ** 2, abs=1e-6)
-        assert rf.pmf(2, 2, p, m=1) == approx(p**2 + 2 * p**2 * (1 - p), abs=1e-6)
+        current = rf.pmf(k=np.array([0, 1, 2]), n=2, p=p, m=1)
+        expected = [(1 - p) ** 2, 2 * p * (1 - p) ** 2, p**2 + 2 * p**2 * (1 - p)]
+        np.testing.assert_allclose(current, expected, atol=1e-6, rtol=0.0)
 
 
 def test_pmf_vector():
@@ -19,6 +18,13 @@ def test_pmf_vector():
     result = rf.pmf(k=np.array([0, 1, 2]), n=2, p=0.1, m=1)
     assert isinstance(result, np.ndarray)
     assert len(result) == 3
+
+
+def test_pmf_large():
+    current = rf.pmf_large(k=np.array([0, 10, 50, 90]), n=100, lambda_=1.5, i_n=1)
+    np.testing.assert_allclose(
+        current, [2.383925e-07, 8.889213e-06, 2.349612e-02, 1.623636e-03], rtol=1e-6
+    )
 
 
 def test_large_dist_warning():
