2737157707e66ec1bbbb0a834f85c89fb6b16d3b
Scott Olesen <ulp7@cdc.gov>
2025-02-27 20:28:37 -0500

    Streamlit app

---
+++ app.py
@@ -0,0 +1,49 @@
+import altair as alt
+import numpy as np
+import polars as pl
+import streamlit as st
+
+import reedfrost as rf
+
+
+def app():
+    st.title("Reed-Frost model")
+    n_susceptible = st.slider(
+        "No. initially susceptible", min_value=0, max_value=50, step=1, value=10
+    )
+    n_infected = st.slider(
+        "No. initially infected", min_value=1, max_value=10, step=1, value=1
+    )
+    p = (
+        st.slider(
+            "Prob. of infection",
+            min_value=0.0,
+            max_value=100.0,
+            step=1.0,
+            format="%d%%",
+            value=5.0,
+        )
+        / 100
+    )
+
+    k = np.array(range(n_susceptible + 1))
+    dens = rf.pmf(k=k, n=n_susceptible, p=p, m=n_infected)
+
+    st.altair_chart(
+        alt.Chart(pl.DataFrame({"k": k, "dens": dens * 100}))
+        .properties(title=f"R_eff={round(n_susceptible * p, 2)}")
+        .encode(
+            alt.X(
+                "k:N",
+                title="Additional no. infected",
+                # scale=alt.Scale(domain=[0, n_susceptible]),
+                # axis=alt.Axis(tickMinStep=1),
+            ),
+            alt.Y("dens", title="Probability (%)"),
+        )
+        .mark_bar()
+    )
+
+
+if __name__ == "__main__":
+    app()
