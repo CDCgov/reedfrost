ab72afcc21567e09ca0c991bdc926c8ee19a5f56
Scott Olesen <ulp7@cdc.gov>
2025-08-21 20:04:15 +0000

    Separate input configuration from layout (#54)
    * Move derived parameters into model
    
    * Separate inputter
    
    * Separate registration from placement

--- src/reedfrost/app.py
+++ src/reedfrost/app.py
@@ -1,3 +1,8 @@
+import importlib.resources
+import json
+from types import ModuleType
+from typing import Any, Callable
+
 import altair as alt
 import numpy as np
 import numpy.random
@@ -7,15 +12,92 @@ from streamlit.delta_generator import DeltaGenerator
 
 import reedfrost
 
+type Inputs = dict[str, Any]
+
+
+class Component:
+    def __init__(self, exec: Callable):
+        """
+        Define a general component that can be called in Streamlit
+        """
+        self.exec = exec
+
+    def __call__(self, inputs: Inputs) -> Any:
+        return self.exec(inputs)
+
+
+class StreamlitComponent(Component):
+    def __init__(self, method_name: str, kwargs_template: dict):
+        """
+        Define a Streamlit component, without actually executing it
+        """
+        assert method_name in dir(st)
+
+        self.method_name = method_name
+        self.kwargs_template = kwargs_template
+
+    def __call__(self, inputs: Inputs, c: DeltaGenerator | ModuleType = st):
+        kwargs = {
+            k: v(inputs) if callable(v) else v for k, v in self.kwargs_template.items()
+        }
+        return getattr(c, self.method_name)(**kwargs)
+
+
+class Inputter:
+    def __init__(self):
+        """
+        Track streamlit input components and the values they return
+        """
+        self.components = {}
+        self.inputs = {}
+
+    def register_component(self, key: str, component: Component):
+        assert key not in self.components
+        self.components[key] = component
+
+    def place_component(self, key: str, c: DeltaGenerator | ModuleType = st):
+        """
+        Args:
+            key: Component ID
+            c: If a DeltaGenerator, the component will be placed in that container.
+                Default is `st`, which will place in the app relative to where it
+                is called in the script.
+        """
+        component = self.components[key]
+        if isinstance(component, StreamlitComponent):
+            self.inputs[key] = component(self.inputs, c=c)
+        elif isinstance(component, Component):
+            self.inputs[key] = component(self.inputs)
+        else:
+            raise RuntimeError()
+
+    def inset_input_value(self, key: str, value):
+        assert key not in self.inputs
+        self.inputs[key] = value
+
 
 def app():
+    inputter = register_inputs()
+
+    # set up app input layout and collect input values ------------------------
     st.set_page_config(
         page_title="Chain binomial models", page_icon="🧮", layout="wide"
     )
     st.title("Chain binomial models")
 
     with st.sidebar:
-        params = get_params()
+        inputter.place_component("n")
+        inputter.place_component("n_immune")
+        inputter.place_component("brn")
+        inputter.place_component("model")
+        inputter.place_component("result_type")
+        inputter.place_component("metric")
+
+        st.header("Input parameters")
+        with st.expander("Advanced options", expanded=False):
+            inputter.place_component("n_infected")
+            inputter.place_component("n_simulations")
+            inputter.place_component("seed")
 
         st.divider()
         st.header("Links")
@@ -25,103 +107,158 @@ def app():
             "https://cdcgov.github.io/reedfrost/", label="documentation", icon="📝"
         )
 
-    results = get_results(params)
-    view(params, results)
+    # run the simulations/computations ----------------------------------------
+    results = get_results(inputter.inputs)
+
+    # render the results ------------------------------------------------------
+    view(inputter.inputs, results)
 
 
-def get_params() -> dict:
-    st.header("Input parameters")
-    n = st.slider("Population size", min_value=1, max_value=100, step=1, value=10)
+def register_inputs() -> Inputter:
+    inputter = Inputter()
+    inputter.register_component(
+        "n",
+        StreamlitComponent(
+            "slider",
+            {
+                "label": "Population size",
+                "min_value": 1,
+                "max_value": 100,
+                "step": 1,
+                "value": 10,
+            },
+        ),
+    )
 
     # user input is in proportions, but we get the integer number
-    n_immune = st.select_slider(
-        "Proportion initially immune",
-        # values are from 0 to N-1, leaving space for at least 1 infected
-        options=range(0, n),
-        value=0,
-        format_func=lambda x: f"{x / n:.0%}",
+    inputter.register_component(
+        "n_immune",
+        StreamlitComponent(
+            "select_slider",
+            {
+                "label": "Proportion initially immune",
+                # values are from 0 to N-1, leaving space for at least 1 infected
+                "options": lambda p: range(0, p["n"]),
+                "value": 0,
+                "format_func": lambda p: lambda x: f"{x / p['n']:.0%}",
+            },
+        ),
     )
 
-    brn = st.slider(
-        "Basic reproduction number",
-        min_value=0.0,
-        max_value=min(15.0, float(n)),
-        step=0.1,
-        format="%.1f",
-        value=min(1.5, float(n)),
+    inputter.register_component(
+        "brn",
+        StreamlitComponent(
+            "slider",
+            {
+                "label": "Basic reproduction number",
+                "min_value": 0.0,
+                "max_value": lambda p: min(15.0, float(p["n"])),
+                "step": 0.1,
+                "format": "%.1f",
+                "value": lambda p: min(1.5, float(p["n"])),
+            },
+        ),
     )
 
-    model = st.segmented_control(
-        "Model",
-        options=["Reed-Frost", "Enko", "Greenwood"],
-        default="Reed-Frost",
+    inputter.register_component(
+        "model",
+        StreamlitComponent(
+            "segmented_control",
+            {
+                "label": "Model",
+                "options": ["Reed-Frost", "Enko", "Greenwood"],
+                "default": "Reed-Frost",
+            },
+        ),
     )
-    assert model is not None
 
-    result_type = st.segmented_control(
-        "Results type",
-        options=["Trajectories", "Theoretical"],
-        default="Trajectories",
+    inputter.register_component(
+        "result_type",
+        StreamlitComponent(
+            "segmented_control",
+            {
+                "label": "Results type",
+                "options": ["Trajectories", "Theoretical"],
+                "default": "Trajectories",
+            },
+        ),
     )
-    assert result_type is not None
 
-    metric = st.segmented_control(
-        "Infections metric",
-        options=["Cumulative", "Incident"],
-        default="Cumulative",
+    inputter.register_component(
+        "metric",
+        StreamlitComponent(
+            "segmented_control",
+            {
+                "label": "Infections metric",
+                "options": ["Cumulative", "Incident"],
+                "default": "Cumulative",
+            },
+        ),
     )
-    assert metric is not None
 
-    with st.expander("Advanced options", expanded=False):
-        # need special handling for the case where everyone is immune but 1,
-        # because streamlit sliders must have a range
-        if n - n_immune == 1:
-            n_infected = 1
-            st.text("No. initially infected: 1")
-        else:
-            n_infected = st.slider(
-                "No. initially infected",
-                min_value=1,
-                max_value=n - n_immune,
-                step=1,
-                value=1,
-            )
+    inputter.register_component("n_infected", Component(_exec_n_infected))
 
-        n_simulations = st.slider(
-            "No. simulations",
-            min_value=5,
-            max_value=250,
+    inputter.register_component(
+        "n_simulations",
+        StreamlitComponent(
+            "slider",
+            {
+                "label": "No. simulations",
+                "min_value": 5,
+                "max_value": 250,
+                "step": 1,
+                "value": 100,
+            },
+        ),
+    )
+
+    inputter.register_component(
+        "seed",
+        StreamlitComponent(
+            "number_input",
+            {
+                "label": "Random seed",
+                "min_value": 0,
+                "max_value": 2**32 - 1,
+                "step": 1,
+                "value": 42,
+            },
+        ),
+    )
+
+    return inputter
+
+
+def _exec_n_infected(inputs: dict) -> int:
+    # need special handling for the case where everyone is immune but 1,
+    # because streamlit sliders must have a range
+    if inputs["n"] - inputs["n_immune"] == 1:
+        st.text("No. initially infected: 1")
+        return 1
+    else:
+        return st.slider(
+            "No. initially infected",
+            min_value=1,
+            max_value=inputs["n"] - inputs["n_immune"],
             step=1,
-            value=100,
+            value=1,
         )
 
-        seed = st.number_input(
-            "Random seed",
-            min_value=0,
-            max_value=2**32 - 1,
-            step=1,
-            value=42,
-        )
 
-    # derived parameters ------------------------------------------------------
-    n_susceptible = n - n_immune - n_infected
-    assert n_susceptible > 0
+@st.cache_resource
+def load_inputs() -> dict:
+    with importlib.resources.open_text("reedfrost", "inputs.json") as f:
+        inputs = json.load(f)
 
-    return {
-        "model": model,
-        "n_susceptible": n_susceptible,
-        "n_infected": n_infected,
-        "n_immune": n_immune,
-        "brn": brn,
-        "n": n,
-        "n_simulations": n_simulations,
-        "seed": seed,
-        "result_type": result_type,
-        "metric": metric,
-    }
+    return inputs
 
 
 def get_results(params) -> dict:
+    # derive parameters
+    n_susceptible = params["n"] - params["n_immune"] - params["n_infected"]
+    assert n_susceptible > 0
+    params["n_susceptible"] = n_susceptible
+
     match (params["result_type"], params["metric"]):
         case ("Trajectories", _):
             return model_trajectories(params)
